26b03b6325379319328292699cfafdadb61b0ed5
Daniel Domingues <ddomingues@minsait.com>
2023-01-26 17:39:42 -0300

    Including CliBuilder reference and adding Option class to process command lind arguments

--- src/main/groovy/br/com/vivo/Dboss.groovy
+++ src/main/groovy/br/com/vivo/Dboss.groovy
@@ -1,22 +1,36 @@
 package br.com.vivo
 
+// https://mvnrepository.com/artifact/org.codehaus.groovy/groovy-cli-commons
+@Grapes(
+        @Grab(group='org.codehaus.groovy', module='groovy-cli-commons', version='3.0.14')
+)
+
+import groovy.cli.commons.CliBuilder
+
 class Dboss {
 
     static void main(String[] args) {
 
-        def ret = new WorkFlow().execute(Arrays.asList(args))
+        HashMap options = new Options().getOptions(args)
 
-        System.out.println(ReturnCodeMessage.geMessageByValue(ret))
+        options.println()
 
-        System.exit(ret)
+        //def ret = new WorkFlow().execute(options)
+
+        //System.out.println(ReturnCodeMessage.geMessageByValue(ret))
+
     }
 }
 
 class WorkFlow {
 
-    int execute(List<String> args) {
+    int execute(HashMap options) {
 
-        return new Validation().validate(args)
+        options.forEach {
+            it.println()
+        }
+
+        //return new Validation().validateInputParameters(args)
 
     }
 
@@ -24,9 +38,9 @@ class WorkFlow {
 
 class Validation {
 
-    int validate(List<String> args) {
+    int validateInputParameters(List<String> parameters) {
 
-        args.forEach {
+        parameters.forEach {
             System.out.println(it.toString())
         }
 
@@ -48,8 +62,9 @@ enum ReturnCodeMessage {
     private final int value
     private final String message
 
-    public int value() {return value}
-    public String message() {return message}
+    public int value() { return value }
+
+    public String message() { return message }
 
     public static String geMessageByValue(int value) {
         for (ReturnCodeMessage e : values()) {
@@ -58,6 +73,61 @@ enum ReturnCodeMessage {
             }
         }
         return null;
+    }
+
+
+}
+
+class Options {
+
+    HashMap getOptions(String[] args) {
+
+        def cli = new CliBuilder(usage: 'groovy Dboss.groovy -u= -p=')
+
+        /*
+        vGitRepository=$1
+        --vgitUser
+        --vGitPassword=$3
+        vGitBranch=$4
+        vDatabaseEnv=$5
+        vDatabaseUser=$6
+        vDatabasePassword=$7
+        vOperation=$8
+        vRelease=$9
+        vProjectID=${10}
+        */
+
+        cli.with {
+            u longOpt: 'gitUser', args: 1, argName: 'gitUser', required: true, 'Git user name'
+            p longOpt: 'gitPassword', args: 1, argName: 'gitPassword', required: true, 'Git password'
+        }
+
+        def optionMap = [:]
+        def options = cli.parse(args)
+        if (!options) {
+            return optionMap
+        }
+
+        optionMap["gitUser"] = options.u ?: options.gitUser
+        optionMap["gitPassword"] = options.p ?: options.gitPassword
+
+        println("OPtionsMap size:" + optionMap.size())
+        println(optionMap.get("gitUser"))
+        println(optionMap.get("gitPassword"))
+
+        return optionMap
+
+    }
+
+}
+
+
+class Util {
+
+    static splitStringBySign(String sign, String value) {
+
+        return Arrays.asList(value.split(sign, value))
+
     }
 
 }
